705e2a990658b03447fb5b5413e76a73508006e9
patrickgsmith <patrickgromersmith@gmail.com>
2019-09-15 19:13:36 -0700

    changed file structure

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('C:/Users/Patrick/Desktop/Gatsby/fb-editor/node_modules/gatsby-plugin-offline/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('C:/Users/Patrick/Desktop/Gatsby/fb-editor/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
--- .cache/async-requires.js
+++
@@ -1,9 +0,0 @@
-// prefer default export if available
-const preferDefault = m => m && m.default || m
-
-exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": () => import("C:\\Users\\Patrick\\Desktop\\Gatsby\\fb-editor\\node_modules\\gatsby-plugin-offline\\app-shell.js" /* webpackChunkName: "component---node-modules-gatsby-plugin-offline-app-shell-js" */),
-  "component---src-pages-404-js": () => import("C:\\Users\\Patrick\\Desktop\\Gatsby\\fb-editor\\src\\pages\\404.js" /* webpackChunkName: "component---src-pages-404-js" */),
-  "component---src-pages-index-js": () => import("C:\\Users\\Patrick\\Desktop\\Gatsby\\fb-editor\\src\\pages\\index.js" /* webpackChunkName: "component---src-pages-index-js" */)
-}
-
