688c31e434132151fd18ffd47a356c77091d4868
smithpg <patrickgromersmith@gmail.com>
2020-03-30 00:09:42 +0000

    remove logging

--- data/db.js
+++ data/db.js
@@ -1,10 +1,11 @@
 const Sequelize = require("sequelize");
 
-const STORAGE_LOCATION = process.env.NODE_ENV === 'test' ? './test.db' : './data/nanowrimo.db'
+const STORAGE_LOCATION = process.env.NODE_ENV === 'test' ? ':memory:' : './data/nanowrimo.db'
 
 const db = new Sequelize({
     dialect: "sqlite",
-    storage: STORAGE_LOCATION
+    storage: STORAGE_LOCATION,
+    logging: false
 });
 
 module.exports = db;
