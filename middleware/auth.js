5c3296c87fd3deb715b52b691712cd713fd795f2
smithpg <patrickgromersmith@gmail.com>
2020-04-08 17:39:23 -0700

    fixed formatting

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,25 +1,23 @@
 const createError = require("http-errors");
 
 module.exports = {
-    redirectIfUnauthenticated: (req, res, next) => {
-        // If the user is not logged in, redirect
-        if (!req.user) {
-            res.redirect("/nanowrimo/auth/login")
-        } else {
-            // Otherwise, continue to next middleware
-            next();
-        }
-    },
+  redirectIfUnauthenticated: (req, res, next) => {
+    // If the user is not logged in, redirect
+    if (!req.user) {
+      res.redirect("/nanowrimo/auth/login");
+    } else {
+      // Otherwise, continue to next middleware
+      next();
+    }
+  },
 
-    errorOnUnauthenticated: (req, res, next) => {
-
-        // If the user is not logged in, redirect
-        if (!req.user) {
-
-            next(createError(401))
-        } else {
-            // Otherwise, continue to next middleware
-            next();
-        }
-    },
-}
+  errorOnUnauthenticated: (req, res, next) => {
+    // If the user is not logged in, redirect
+    if (!req.user) {
+      next(createError(401));
+    } else {
+      // Otherwise, continue to next middleware
+      next();
+    }
+  }
+};
