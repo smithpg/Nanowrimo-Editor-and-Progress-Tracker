de03bb1ea9909c3733b835526b561a3c3fa59ac9
smithpg <patrickgromersmith@gmail.com>
2020-04-08 17:39:06 -0700

    bugfix

--- routers/api.js
+++ routers/api.js
@@ -1,56 +1,66 @@
 const { Router } = require("express");
-const { User, Document } = require("../data")
+const { User, Document } = require("../data");
 
-// Create api router that handles login routes, plus document 
+// Create api router that handles login routes, plus document
 // retrieval
 
 const apiRouter = new Router();
 
 apiRouter.get("/user", async (req, res, next) => {
-    // Respond with user info and list of documents
-    const documents = await Document.findAll({ where: { ownerId: req.user.googleId } });
+  // DELETE THIS
+  // res.json({
+  //   firstname: "John",
+  //   lastname: "Doe",
+  //   email: "jdoe@gmail.com",
+  //   documents: [
+  //     { title: "My Diary", content: "lorem ipsum", ownerId: 123, id: 123 }
+  //   ]
+  // });
 
-    const userInfo = {
-        ...req.user,
-        documents
-    }
+  // Respond with user info and list of documents
+  const documents = await Document.findAll({
+    where: { ownerId: req.user.get("googleId") }
+  });
 
-    res.send(userInfo);
-})
+  const userInfo = {
+    ...req.user.dataValues,
+    documents
+  };
+
+  res.send(userInfo);
+});
 
 apiRouter.post("/document", (req, res, next) => {
-    // Create new document
-    const docOpts = req.body;
-
-    Document.create(docOpts).then(createdDoc => {
-        res.json(createdDoc.get)
-    });
-})
+  // Create new document
+  const docOpts = { ...req.body, ownerId: req.user.get("googleId") };
+  console.log(docOpts);
+  Document.create(docOpts).then(createdDoc => {
+    res.json(createdDoc.get({ plain: true }));
+  });
+});
 
 apiRouter
-    .param("document_id", async (req, res, next, docId) => {
-        try {
-            req.retrievedDocument = await Document.findByPk(docId);
-            next();
-        } catch (error) {
-            next(error)
-        }
-    })
-    .route("/document/:document_id")
-    .get(async (req, res, next) => {
-        res.json(req.retrievedDocument);
-    })
-    .put(async (req, res, next) => {
-        const update = req.body;
+  .param("document_id", async (req, res, next, docId) => {
+    try {
+      req.retrievedDocument = await Document.findByPk(docId);
+      next();
+    } catch (error) {
+      next(error);
+    }
+  })
+  .route("/document/:document_id")
+  .get(async (req, res, next) => {
+    res.json(req.retrievedDocument);
+  })
+  .put(async (req, res, next) => {
+    const update = req.body;
 
-        // Apply update to document instance
-        Object.assign(req.retrievedDocument, update);
-        await req.retrievedDocument.save();
+    // Apply update to document instance
+    Object.assign(req.retrievedDocument, update);
+    await req.retrievedDocument.save();
 
-        // Respond with updated document
-        res.json(req.retrievedDocument);
-    })
-
-
+    // Respond with updated document
+    res.json(req.retrievedDocument);
+  });
 
 module.exports = apiRouter;
